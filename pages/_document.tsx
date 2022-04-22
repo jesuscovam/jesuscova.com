4cbebd2d17a29320b0991b0174c1e9f6eea07dda
Jesus Cova <jesuscovam@gmail.com>
2022-04-21 19:52:13 -0500

    titulo y favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>jesus</title>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐸</text></svg>"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ function Section({ children }: { children: ReactNode }): JSX.Element {
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-black font-mono text-white ">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black py-4 font-mono text-white">
       <Section>
         <Contact />
       </Section>

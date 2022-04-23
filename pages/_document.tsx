754f9198400225b9f8dee7ead96bbaddc43c604d
Jesus Cova <jesuscovam@gmail.com>
2022-04-23 13:08:36 -0500

    titulo de pagina

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>jesus</title>
           <link
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐸</text></svg>"
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Code } from '@/components/code'
 import { Songs } from '@/components/songs'
 import type { NextPage } from 'next'
 import { Contact } from '@/components/contact'
+import Head from 'next/head'
 
 function Section({ children }: { children: ReactNode }): JSX.Element {
   return <div className="w-60">{children}</div>
@@ -10,31 +11,36 @@ function Section({ children }: { children: ReactNode }): JSX.Element {
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-4  ">
-      <Section>
-        <Contact />
-      </Section>
+    <>
+      <Head>
+        <title>jesus</title>
+      </Head>
+      <div className="flex min-h-screen flex-col items-center justify-center py-4  ">
+        <Section>
+          <Contact />
+        </Section>
 
-      <div className=" mt-5 space-y-14  bg-opacity-90 py-2">
-        <Section>
-          <Code />
-        </Section>
-        <Section>
-          <Songs />
-        </Section>
+        <div className=" mt-5 space-y-14  bg-opacity-90 py-2">
+          <Section>
+            <Code />
+          </Section>
+          <Section>
+            <Songs />
+          </Section>
+        </div>
+        <footer className="mt-10">
+          <Section>
+            <a
+              href="https://github.com/jesuscovam/jesuscova.com"
+              target="_blank"
+              className="text-indigo-400 underline"
+            >
+              code from this website
+            </a>
+          </Section>
+        </footer>
       </div>
-      <footer className="mt-10">
-        <Section>
-          <a
-            href="https://github.com/jesuscovam/jesuscova.com"
-            target="_blank"
-            className="text-indigo-400 underline"
-          >
-            code from this website
-          </a>
-        </Section>
-      </footer>
-    </div>
+    </>
   )
 }
 
